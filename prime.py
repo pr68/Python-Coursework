4277e2555de6467c955d38a6fda8aa126996f343
pr68 <rogersp3@cardiff.ac.uk>
2017-11-16 14:40:51 +0000

    Changed prime

--- prime.py
+++ prime.py
@@ -1,25 +1,34 @@
 import math
-file = open("numbers.txt", "r")
-st = file.read()
-stt = st.replace(",", "")
-sstt = stt.split()
-numbers = list(map(int, sstt))
-print(numbers)
 
-for i in numbers:
-    if i % 2 == 0:
-        print(i,"is not a prime number")
-    elif i % 3 == 0:
-        print(i,"is not a prime number")
-    elif i % 5 == 0:
-        print(i,"is not a prime number")
-    elif i > 1:
-        for n in range(2,int(math.sqrt(i)) + 1,1):
-            if (i%n) == 0:
-                print(i,"is not a prime number")
-                break
+
+def readnumbers(filename):
+    file = open(filename, "r")
+    st = file.read()
+    stt = st.replace(",", "")
+    sstt = stt.split()
+    numbers = list(map(int, sstt))
+    print(numbers)
+
+
+def isprime(filename):
+    readnumbers(filename)
+    for i in numbers:
+        if i % 2 == 0:
+            print(i, "is not a prime number")
+        elif i % 3 == 0:
+            print(i, "is not a prime number")
+        elif i % 5 == 0:
+            print(i, "is not a prime number")
+        elif i > 1:
+            for n in range(2, int(math.sqrt(i)) + 1, 1):
+                if (i % n) == 0:
+                    print(i, "is not a prime number")
+                    break
+            else:
+                print(i, "is a prime number")
+
         else:
-            print(i,"is a prime number")
+            print(i, "is not a prime number")
 
-    else:
-        print(i,"is not a prime number")
+
+isprime("numbers.txt")
